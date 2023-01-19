a1b30f93626e551d5ac4ea32112f85bdde97c88c
yaashh8 <yashvbhakta18@gmail.com>
2023-01-19 18:16:39 +0530

    codes

---
+++ PR-4/EX-4-18.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+main() {
+	int i,j,s;
+	for(i=1;i<=5;i++){
+		for(j=1;j<=i;j++){
+			printf("%d",j);
+		}printf("\n");
+		for(s=1;s<=5-i;s++){
+			printf(" ");
+		}
+		for(s=1;s<=5-i;s++){
+			printf(" ");
+		}	
+		for(j=i;j>=1;j--){
+			printf("%d",j);
+		}printf("\n");
+		
+		
+	}
+}
---
+++ PR-4/EX-4-22.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+main(){
+	char i,j;
+	for(i='A';i<='E';i++){
+		for(j='A';j<=i;j++){
+			printf("%c",i);
+		}printf("\n");
+	}
+}
