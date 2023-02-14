388a7881e860d705259fd68ce435b852d34a7b45
R11\R11 <chintandodiya28@gmail.com>
2023-02-14 16:40:49 +0530

    file handling

---
+++ ch-14/YB35-14-1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+struct student{
+	int id;
+	char name[20];
+	float per;
+};
+
+void main(){
+	FILE *fp,*fp2;
+	
+	struct student s;
+	
+	fp = fopen("C:\\Users\\R11\\Desktop\\IPF.txt","r");
+	
+	if(fp!=NULL){
+		fscanf(fp,"%d%s%f",&s.id,&s.name,&s.per);
+		printf("data of student\nID\t:%d\nNAME\t:%s\nPER\t:%.2f\n",s.id,s.name,s.per);
+	} else{
+		printf("file opending failed !!");
+	}
+	
+	fp2 = fopen("C:\\Users\\R11\\Desktop\\NF.txt","a");
+	
+	if(fp2!=NULL){
+		fprintf(fp2,"data of student\nID\t:%d\nNAME\t:%s\nPER\t:%.2f\n",s.id,s.name,s.per);
+	} else{
+		printf("file opending failed !!");
+	}
+	
+	fclose(fp);
+	fclose(fp2);
+	
+}
